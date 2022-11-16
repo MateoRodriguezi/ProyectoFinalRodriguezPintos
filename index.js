f6c91c26bc50f7aa66596ef2580770da2d5f275a
Mateo Rodriguez <mrodriguez@prometeoapi.com>
2022-11-16 18:41:43 -0300

    cambios en la estructura y orden del index.js

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-let ingresoAcumulado = 0;
-let acumuladorGastos = 0;
-let detalleIngreso = [];
-let detalleGasto = [];
 let usuarios = [];
 
 function saldo(ingresos,gastos){
     return ingresos - gastos;
 }
 
+class NuevoUsuario {
+    constructor (nombre, email, edad ,password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+        this.password = password;
+    }
+        esMayor = () => {
+            if (this.edad >= 18) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+const user1 = new NuevoUsuario ('Mateo', 'mateo@gmail.com', 25, 'mateo1234')
+usuarios.push(user1)
+const user2 = new NuevoUsuario ('Hernan', 'hernan@gmail.com', 31, 'hernan5678' )
+usuarios.push(user2)
+const user3 = new NuevoUsuario ('Julia', 'julia@gmail.com', 14 , 'Julia34521' )
+usuarios.push(user3)
+
+const filtrado = usuarios.filter(e => e.edad > 18)
+
+let detalleIngreso = [];
+let detalleGasto = [];
+let ingresoAcumulado = 0;
+let acumuladorGastos = 0;
 
 for(let i=1; i<5; i++){
     alert("Describa el ingreso N° " + i)
@@ -57,56 +82,10 @@ for(let i=1; i<5; i++){
     }
 }
 
-// CONSOLE.LOG PARA VER LOS ARRAY EN LA CONSOLA
-console.log(detalleIngreso)
-console.log(detalleGasto)
-
-// OBJETOS CREADOS - PARA FUTUROS USUARIOS DE LA PLATAFORMA
-
-class NuevoUsuario {
-    constructor (nombre, email, edad ,password) {
-        this.nombre = nombre;
-        this.email = email;
-        this.edad = edad;
-        this.password = password;
-    }
-        esMayor () {
-            if (this.edad >= 18) {
-                return true
-            } else {
-                return false
-            }
-        }
-    }
-
-const user1 = new NuevoUsuario ('Mateo', 'mateo@gmail.com', 25, 'mateo1234')
-usuarios.push(user1)
-const user2 = new NuevoUsuario ('Hernan', 'hernan@gmail.com', 31, 'hernan5678' )
-usuarios.push(user2)
-const user3 = new NuevoUsuario ('Julia', 'julia@gmail.com', 14 , 'Julia34521' )
-usuarios.push(user3)
-
-console.log(user1,user2, user3)
-console.log("El usuario " + user1.nombre + " es mayor? " + user1.esMayor())
-console.log("El usuario " + user2.nombre + " es mayor? " + user2.esMayor())
-console.log("El usuario " + user3.nombre + " es mayor? " + user3.esMayor())
-
-
-// ALERT QUE MUESTRA LA FUNCION CREADA (SALDO)
-
 alert(
     "El total de tus ingresos es de: $ " + ingresoAcumulado +"\n"+
     "Los gastos de este mes, suman: $ " + acumuladorGastos +"\n"+
     "Tu saldo disponible es de: $ " + saldo( ingresoAcumulado,acumuladorGastos));
-
-
-// ALERT PARA VER LOS OBJETOS Y ARRAYS EN LA CONSOLA
-    
-alert(
-    "Puede ver los objetos usuarios creados en la consola!");
-
-
-// ALERT PARA VER LOS DETALLES DE INGRESOS Y GASTOS
 
 let ingresosJoin = detalleIngreso.join(' , ')
 alert("Los ingresos fueron: " + ingresosJoin)
@@ -114,21 +93,18 @@ alert("Los ingresos fueron: " + ingresosJoin)
 let gastosJoin = detalleGasto.join(' , ')
 alert("Los gastos fueron: " + gastosJoin)
 
-// ALERT PARA VER SI EL USUARIO COBRO EL SUELDO
-
 let index = detalleIngreso.indexOf('SUELDO')
-console.log(index)
 if (index !== -1) {
     alert ('Ya has cobrado tu sueldo!')
 } else {
     alert ('Aun no has cobrado tu sueldo!')
 }
 
-// CONSOLE LOG DEL ARRAY DE USUARIOS Y METODOS AVANZADOS DE ARRAY
-
+console.log(detalleIngreso)
+console.log(detalleGasto)
+console.log(user1,user2, user3)
+console.log("El usuario " + user1.nombre + " es mayor? " + user1.esMayor())
+console.log("El usuario " + user2.nombre + " es mayor? " + user2.esMayor())
+console.log("El usuario " + user3.nombre + " es mayor? " + user3.esMayor())
 console.log(usuarios)
-
-// FILTRA A LOS USUARIOS MAYORES DE EDAD
-
-const filtrado = usuarios.filter(e => e.edad > 18)
 console.log (filtrado)
